2b703dc602ae7dc62078f6c4d0085fb4ab320050
Erik Salvatier <m.erik.salvatier@gmail.com>
2018-10-01 18:25:14 -0700

    fire.auth functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,6 +13,23 @@ $(document).ready(function () {
   var currentUser;
   var assetList = [];
   var liabList = [];
+  function loginForm() {
+    $("#userLogSection").empty();
+      var newLoginForm = $("<form>");
+      var inputEmailForm = $("<div>").addClass("input-group");
+      var emailLabel = $("<label>").addClass("input-group-text").attr("for", "userEmail").text("Email");
+      var emailInput = $("<input>").attr({"type": "text","id": "userEmail"});
+      inputEmailForm.append(emailLabel, emailInput);
+      var inputPassForm = $("<div>").addClass("input-group");
+      var passLabel = $("<label>").addClass("input-group-text").attr("for", "userPassword").text("Password");
+      var passInput = $("<input>").attr({"type": "password","id": "userPassword"});
+      inputPassForm.append(passLabel, passInput);
+      var signInBtn = $("<button>").attr({"type": "submit", "id": "signinUser"}).addClass("btn btn-outline-primary").text("Sign-In");
+      var newUsrBtn = $("<button>").attr({"type": "submit", "id": "newUserSubmit"}).addClass("btn btn-outline-primary").text("Submit New User");
+      newLoginForm.append(inputEmailForm, inputPassForm, signInBtn, newUsrBtn);
+      $("#userLogSection").append(newLoginForm);
+      $("#budgetTable > tbody").empty();
+  }
 
   function drawTable(user) {
     $("#budgetTable > tbody").empty();
@@ -46,42 +63,69 @@ $(document).ready(function () {
       }
       var netRow = $("<tr>");
       var netCell = $("<td>").text(eval(totalAsset - totalLiab));
-      netRow.append( $("<td>"), $("<td>"), netCell);
+      netRow.append($("<td>"), $("<td>"), netCell);
       $("#budgetTable").append(netRow);
     });
   }
 
-  $(document.body).on("click", "#userSubmit", function (event) {
-    console.log("happening");
-    event.preventDefault();
-    var temp = $("#userName").val().trim();
-    var passed = false;
-    database.ref().once("value", function () {}).then(function (snapShot) {
-      snapShot.forEach(function (current) {
-        if (current.val().name == temp) {
-          console.log("pass");
-          currentUser = current.key;
-          passed = true;
-        }
-      });
-      if (!passed) {
-        console.log("new");
-        var newUserRef = database.ref().push();
-        newUserRef.update({
-          name: temp,
-          liabilities: "[]",
-          assets: "[]",
-          stocks: "[]",
-          crypto: "[]"
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      console.log("signed in")
+      var temp = user.uid;
+      var passed = false;
+      database.ref().once("value", function () {}).then(function (snapShot) {
+        snapShot.forEach(function (current) {
+          if (current.val().uid == temp) {
+            console.log("pass");
+            currentUser = current.key;
+            passed = true;
+          }
         });
-        currentUser = newUserRef;
-      }
-      $("#userDisp").empty();
-      $("button").removeAttr("disabled");
-      var userDisp = $("<h1>").text(temp);
-      $("#userDisp").append(userDisp);
-      console.log(currentUser);
-      drawTable(currentUser);
+        if (!passed) {
+          var newUserRef = database.ref().push();
+          newUserRef.update({
+            uid: temp,
+            liabilities: "[]",
+            assets: "[]",
+            stocks: "[]",
+            crypto: "[]"
+          });
+          currentUser = newUserRef;
+        }
+        $("#userLogSection").empty();
+        $("button").removeAttr("disabled");
+        var userDisp = $("<h3>").text(user.email);
+        var signOut = $("<button>").attr({"type":"submit", "id": "signOutBtn"}).addClass("btn btn-outline-danger").text("Sign-Out");
+        $("#userLogSection").append(userDisp, signOut);
+        drawTable(currentUser);
+      });
+    } else {
+    }
+  });
+
+  $(document.body).on("click", "#signinUser", function (event) {
+    event.preventDefault();
+    var email = $("#userEmail").val().trim();
+    var password = $("#userPassword").val().trim();
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+      // ...
+    });
+  });
+
+  $(document.body).on("click", "#newUserSubmit", function (event) {
+    event.preventDefault();
+    var email = $("#userEmail").val().trim();
+    var password = $("#userPassword").val().trim();
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+      // ...
     });
   });
 
@@ -100,6 +144,7 @@ $(document).ready(function () {
       });
       console.log(tempArr);
     }).then(function () {
+      $("#liabilityInput").text("");
       drawTable(currentUser);
     });
   });
@@ -119,7 +164,18 @@ $(document).ready(function () {
       });
       console.log(tempArr);
     }).then(function () {
+      $("#assetInput").text("");
       drawTable(currentUser);
+    });
+  });
+
+  $(document.body).on("click", "#signOutBtn", function () {
+    firebase.auth().signOut().then(function() {
+      currentUser = undefined;
+      loginForm();
+      console.log(currentUser);
+    }).catch(function(error) {
+      console.log(error);
     });
   });
 });
