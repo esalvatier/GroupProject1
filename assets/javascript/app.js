eebb62ec6168d4fee7fbc5ef8d03bac9c5e3e12c
ricky <ricky@D-10-18-202-90.dhcp4.washington.edu>
2018-10-01 21:10:25 -0700

    added crypto fixed stocks

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,41 +10,44 @@
 //   firebase.initializeApp(config);
 
 // });
+
+var stock = ["AAPL"];
+
 $(document).ready(function () {
 
-  var stock = ["AAPL", "AMZN", "TSLA", null];
   // API Key for Alpha Vantage FNPWI5GFVP98Q8ZL
   // API key for World Trading zCxY4p4XRUfscbHnY2eRflSMKBIccU0PnSTFOrpP6397VQuzMayCp4JpNqUf
 
-  function worldStock() {
+  function queryfunct(arrayTerms, stringTerm) {
+    for (var i = 0; i < arrayTerms.length; i++) {
+      var queryURL = "https://www.worldtradingdata.com/api/v1/stock_search?search_term=" + arrayTerms[i] + "&search_by=symbol,name&limit=20&stock_exchange=NASDAQ,NYSE&page=1&api_token=zCxY4p4XRUfscbHnY2eRflSMKBIccU0PnSTFOrpP6397VQuzMayCp4JpNqUf"
+      $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
 
-    var queryURL = "https://www.worldtradingdata.com/api/v1/stock_search?search_term=" + stock + "&search_by=symbol,name&limit=50&page=1&api_token=zCxY4p4XRUfscbHnY2eRflSMKBIccU0PnSTFOrpP6397VQuzMayCp4JpNqUf"
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-
-      .then(function (response) {
-        console.log(response);
-
-        for (var i = 0; i < stock.length; i++) {
+        .then(function (response) {
+          console.log(response);
           var stockDiv = $("<div class='stock'>")
 
-          var stockName = response.data[i].name;
-          var stockPrice = response.data[i].price;
+          var stockName = response.data[0].name;
+          var stockPrice = response.data[0].price;
 
           var pOne = $("<p>").text(stockName + " USD: $" + stockPrice);
           stockDiv.append(pOne);
 
-          $("#stocks-view").prepend(stockDiv);
+          $(stringTerm).append(stockDiv);
 
           console.log(stockDiv);
-        }
-      });
+        });
+
+    }
+
+
   }
 
   $("#add-stock").on("click", function (event) {
+
     $("#stocks-view").empty();
 
     event.preventDefault();
@@ -53,64 +56,57 @@ $(document).ready(function () {
 
     stock.push(stocks);
 
-    worldStock();
+    queryfunct(stock, "#stocks-view");
   })
-  worldStock();
-});
 
-$(document).ready(function () {
-  var stock = ["AAPL"];
-
-
-  // API Key for Alpha Vantage FNPWI5GFVP98Q8ZL
-  // API key for World Trading zCxY4p4XRUfscbHnY2eRflSMKBIccU0PnSTFOrpP6397VQuzMayCp4JpNqUf
-
-  function searchStock() {
-
-    var queryURL = "https://www.worldtradingdata.com/api/v1/stock_search?search_term=" + stock + "&search_by=symbol,name&limit=50&page=1&api_token=zCxY4p4XRUfscbHnY2eRflSMKBIccU0PnSTFOrpP6397VQuzMayCp4JpNqUf"
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-
-      .then(function (response) {
-        console.log(response);
-
-        for (var i = 0; i < stock.length; i++) {
-          var stockDiv = $("<div class='stock'>")
-
-          var stockName = response.data[i].name;
-          var stockPrice = response.data[i].price;
-
-          var pOne = $("<p>").text(stockName + " USD: $" + stockPrice);
-          stockDiv.append(pOne);
-
-          $("#stocks-view").prepend(stockDiv);
-
-          console.log(stockDiv);
-        }
-      });
-  }
-
-  $("#add-stock").on("click", function (event) {
-    $("#stocks-view").empty();
+  $("#search-stock").on("click", function (event) {
+    $("#search-view").empty();
 
     event.preventDefault();
 
-    
-    //["baseball", "boxing", "bowling", "volleyball", "golf"];
-    var stocks = $("#stock-input").val().trim();
-    var start_index = 0
-    var number_of_elements_to_remove = 1;
-    var removed_elements = stock.splice(start_index, number_of_elements_to_remove, stocks);
-    console.log(removed_elements);
-    //["tennis", "golf"]
+    var stocks = [$("#search-input").val().trim()];
+
     console.log(stock);
 
-    // stock.push(stocks);
-
-    searchStock();
+    queryfunct(stocks, "#search-view");
   })
-  searchStock();
+
+
+  function displayCryptoInfo(crypto) {
+
+    var queryURL = "https://api.coinmarketcap.com/v2/ticker/?limit=3"
+
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+
+      console.log(response);
+
+      var dataobj = response.data;
+      console.log(dataobj);
+
+      for (var tickers in dataobj) {
+        var cryptoDiv = $("#crypto-view");
+
+        var bitName = dataobj[tickers].name;
+        console.log(bitName);
+        var bitPrice = dataobj[tickers].quotes.USD.price;
+        bitPrice = Number(Math.round(bitPrice + 'e2') + 'e-2');
+
+        var cryptOne = $("<p>").text(bitName + " USD: $" + bitPrice);
+        cryptoDiv.append(cryptOne);
+
+        $(crypto).append(cryptoDiv);
+
+        console.log(cryptoDiv);
+      }
+
+    })
+  };
+
+  displayCryptoInfo();
+
+  queryfunct(stock, "#stocks-view");
 })
